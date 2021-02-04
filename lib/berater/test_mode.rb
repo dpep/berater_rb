90abaff4bd8f205501950fa3f263782096f47820
Daniel Pepper <pepper.daniel@gmail.com>
2021-02-04 05:39:47 -0800

    testing mode
    make it easier to use Berater while testing - pass or fail without hitting redis, or just use per usual

---
+++ lib/berater/test_mode.rb
@@ -0,0 +1,43 @@
+require 'berater'
+
+module Berater
+  extend self
+
+  attr_reader :test_mode
+
+  def test_mode=(mode)
+    unless [ nil, :pass, :fail ].include?(mode)
+      raise ArgumentError, "invalid mode: #{Berater.test_mode}"
+    end
+
+    @test_mode = mode
+  end
+
+  class Limiter
+    def self.new(*args, **opts)
+      return super unless Berater.test_mode
+
+      # chose a stub class with desired behavior
+      stub_klass = case Berater.test_mode
+      when :pass
+        Berater::Unlimiter
+      when :fail
+        Berater::Inhibitor
+      end
+
+      # don't stub self
+      return super if self < stub_klass
+
+      # swap out limit method with stub
+      super.tap do |instance|
+        stub = stub_klass.allocate
+        stub.send(:initialize, *args, **opts)
+
+        instance.define_singleton_method(:limit) do |&block|
+          stub.limit(&block)
+        end
+      end
+    end
+  end
+
+end
---
+++ spec/test_mode_spec.rb
@@ -0,0 +1,170 @@
+require 'berater/test_mode'
+
+describe 'Berater.test_mode' do
+  after { Berater.test_mode = nil }
+
+  describe 'Unlimiter' do
+    let(:limiter) { Berater::Unlimiter.new }
+
+    context 'when test_mode = nil' do
+      before { Berater.test_mode = nil }
+
+      it { expect(limiter).to be_a Berater::Unlimiter }
+
+      it 'works per usual' do
+        10.times { expect(limiter.limit).to be_a Berater::Lock }
+      end
+    end
+
+    context 'when test_mode = :pass' do
+      before { Berater.test_mode = :pass }
+
+      it { expect(limiter).to be_a Berater::Unlimiter }
+
+      it 'always works' do
+        10.times { expect(limiter.limit).to be_a Berater::Lock }
+      end
+    end
+
+    context 'when test_mode = :fail' do
+      before { Berater.test_mode = :fail }
+
+      it { expect(limiter).to be_a Berater::Unlimiter }
+
+      it 'never works' do
+        expect { limiter.limit }.to be_overloaded
+      end
+    end
+  end
+
+  describe 'Inhibitor' do
+    let(:limiter) { Berater::Inhibitor.new }
+
+    context 'when test_mode = nil' do
+      before { Berater.test_mode = nil }
+
+      it { expect(limiter).to be_a Berater::Inhibitor }
+
+      it 'works per usual' do
+        expect { limiter.limit }.to be_overloaded
+      end
+    end
+
+    context 'when test_mode = :pass' do
+      before { Berater.test_mode = :pass }
+
+      it { expect(limiter).to be_a Berater::Inhibitor }
+
+      it 'always works' do
+        10.times { expect(limiter.limit).to be_a Berater::Lock }
+      end
+    end
+
+    context 'when test_mode = :fail' do
+      before { Berater.test_mode = :fail }
+
+      it { expect(limiter).to be_a Berater::Inhibitor }
+
+      it 'never works' do
+        expect { limiter.limit }.to be_overloaded
+      end
+    end
+  end
+
+  describe 'RateLimiter' do
+    let(:limiter) { Berater::RateLimiter.new(:key, 1, :second) }
+
+    shared_examples 'a RateLimiter' do
+      it { expect(limiter).to be_a Berater::RateLimiter }
+
+      it 'checks arguments' do
+        expect {
+          Berater::RateLimiter.new(:key, 1)
+        }.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'when test_mode = nil' do
+      before { Berater.test_mode = nil }
+
+      it_behaves_like 'a RateLimiter'
+
+      it 'works per usual' do
+        expect(limiter.redis).to receive(:multi).twice.and_call_original
+        expect(limiter.limit).to be_a Berater::Lock
+        expect { limiter.limit }.to be_overloaded
+      end
+    end
+
+    context 'when test_mode = :pass' do
+      before { Berater.test_mode = :pass }
+
+      it_behaves_like 'a RateLimiter'
+
+      it 'always works and without calling redis' do
+        expect(limiter.redis).not_to receive(:multi)
+        10.times { expect(limiter.limit).to be_a Berater::Lock }
+      end
+    end
+
+    context 'when test_mode = :fail' do
+      before { Berater.test_mode = :fail }
+
+      it_behaves_like 'a RateLimiter'
+
+      it 'never works and without calling redis' do
+        expect(limiter.redis).not_to receive(:multi)
+        expect { limiter.limit }.to be_overloaded
+      end
+    end
+  end
+
+  describe 'ConcurrencyLimiter' do
+    let(:limiter) { Berater::ConcurrencyLimiter.new(:key, 1) }
+
+    shared_examples 'a ConcurrencyLimiter' do
+      it { expect(limiter).to be_a Berater::ConcurrencyLimiter }
+
+      it 'checks arguments' do
+        expect {
+          Berater::ConcurrencyLimiter.new(:key, 1.0)
+        }.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'when test_mode = nil' do
+      before { Berater.test_mode = nil }
+
+      it_behaves_like 'a ConcurrencyLimiter'
+
+      it 'works per usual' do
+        expect(limiter.redis).to receive(:eval).twice.and_call_original
+        expect(limiter.limit).to be_a Berater::Lock
+        expect { limiter.limit }.to be_overloaded
+      end
+    end
+
+    context 'when test_mode = :pass' do
+      before { Berater.test_mode = :pass }
+
+      it_behaves_like 'a ConcurrencyLimiter'
+
+      it 'always works and without calling redis' do
+        expect(limiter.redis).not_to receive(:eval)
+        10.times { expect(limiter.limit).to be_a Berater::Lock }
+      end
+    end
+
+    context 'when test_mode = :fail' do
+      before { Berater.test_mode = :fail }
+
+      it_behaves_like 'a ConcurrencyLimiter'
+
+      it 'never works and without calling redis' do
+        expect(limiter.redis).not_to receive(:eval)
+        expect { limiter.limit }.to be_overloaded
+      end
+    end
+  end
+
+end
