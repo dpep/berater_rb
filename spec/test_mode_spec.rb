85efde1f3a1a035ab37d3ecdefde7a0853ee4bb8
Daniel Pepper <pepper.daniel@gmail.com>
2021-02-25 08:40:47 -0800

    s/limiter/subject/

--- spec/test_mode_spec.rb
+++ spec/test_mode_spec.rb
@@ -4,81 +4,81 @@ describe 'Berater.test_mode' do
   after { Berater.test_mode = nil }
 
   describe 'Unlimiter' do
-    let(:limiter) { Berater::Unlimiter.new }
+    subject { Berater::Unlimiter.new }
 
     context 'when test_mode = nil' do
       before { Berater.test_mode = nil }
 
-      it { expect(limiter).to be_a Berater::Unlimiter }
+      it { is_expected.to be_a Berater::Unlimiter }
 
       it 'works per usual' do
-        expect {|block| limiter.limit(&block) }.to yield_control
-        10.times { expect(limiter.limit).to be_a Berater::Lock }
+        expect {|block| subject.limit(&block) }.to yield_control
+        10.times { expect(subject.limit).to be_a Berater::Lock }
       end
     end
 
     context 'when test_mode = :pass' do
       before { Berater.test_mode = :pass }
 
-      it { expect(limiter).to be_a Berater::Unlimiter }
+      it { is_expected.to be_a Berater::Unlimiter }
 
       it 'always works' do
-        expect {|block| limiter.limit(&block) }.to yield_control
-        10.times { expect(limiter.limit).to be_a Berater::Lock }
+        expect {|block| subject.limit(&block) }.to yield_control
+        10.times { expect(subject.limit).to be_a Berater::Lock }
       end
     end
 
     context 'when test_mode = :fail' do
       before { Berater.test_mode = :fail }
 
-      it { expect(limiter).to be_a Berater::Unlimiter }
+      it { is_expected.to be_a Berater::Unlimiter }
 
       it 'never works' do
-        expect { limiter }.to be_overloaded
+        expect { subject }.to be_overloaded
       end
     end
   end
 
   describe 'Inhibitor' do
-    let(:limiter) { Berater::Inhibitor.new }
+    subject { Berater::Inhibitor.new }
 
     context 'when test_mode = nil' do
       before { Berater.test_mode = nil }
 
-      it { expect(limiter).to be_a Berater::Inhibitor }
+      it { is_expected.to be_a Berater::Inhibitor }
 
       it 'works per usual' do
-        expect { limiter }.to be_overloaded
+        expect { subject }.to be_overloaded
       end
     end
 
     context 'when test_mode = :pass' do
       before { Berater.test_mode = :pass }
 
-      it { expect(limiter).to be_a Berater::Inhibitor }
+      it { is_expected.to be_a Berater::Inhibitor }
 
       it 'always works' do
-        expect {|block| limiter.limit(&block) }.to yield_control
-        10.times { expect(limiter.limit).to be_a Berater::Lock }
+        expect {|block| subject.limit(&block) }.to yield_control
+        10.times { expect(subject.limit).to be_a Berater::Lock }
       end
     end
 
     context 'when test_mode = :fail' do
       before { Berater.test_mode = :fail }
 
-      it { expect(limiter).to be_a Berater::Inhibitor }
+      it { is_expected.to be_a Berater::Inhibitor }
 
       it 'never works' do
-        expect { limiter }.to be_overloaded
+        expect { subject }.to be_overloaded
       end
     end
   end
 
   describe 'RateLimiter' do
-    let(:limiter) { Berater::RateLimiter.new(:key, 1, :second) }
+    subject { Berater::RateLimiter.new(:key, 1, :second) }
 
     shared_examples 'a RateLimiter' do
-      it { expect(limiter).to be_a Berater::RateLimiter }
+      it { is_expected.to be_a Berater::RateLimiter }
 
       it 'checks arguments' do
         expect {
@@ -94,12 +94,12 @@ describe 'Berater.test_mode' do
 
       it 'works per usual' do
         expect(Berater::RateLimiter::LUA_SCRIPT).to receive(:eval).twice.and_call_original
-        expect(limiter.limit).to be_a Berater::Lock
-        expect { limiter.limit }.to be_overloaded
+        expect(subject.limit).to be_a Berater::Lock
+        expect { subject.limit }.to be_overloaded
       end
 
       it 'yields per usual' do
-        expect {|block| limiter.limit(&block) }.to yield_control
+        expect {|block| subject.limit(&block) }.to yield_control
       end
     end
 
@@ -110,8 +110,8 @@ describe 'Berater.test_mode' do
 
       it 'always works and without calling redis' do
         expect(Berater::RateLimiter::LUA_SCRIPT).not_to receive(:eval)
-        expect {|block| limiter.limit(&block) }.to yield_control
-        10.times { expect(limiter.limit).to be_a Berater::Lock }
+        expect {|block| subject.limit(&block) }.to yield_control
+        10.times { expect(subject.limit).to be_a Berater::Lock }
       end
     end
 
@@ -122,16 +122,16 @@ describe 'Berater.test_mode' do
 
       it 'never works and without calling redis' do
         expect(Berater::RateLimiter::LUA_SCRIPT).not_to receive(:eval)
-        expect { limiter }.to be_overloaded
+        expect { subject }.to be_overloaded
       end
     end
   end
 
   describe 'ConcurrencyLimiter' do
-    let(:limiter) { Berater::ConcurrencyLimiter.new(:key, 1) }
+    subject { Berater::ConcurrencyLimiter.new(:key, 1) }
 
     shared_examples 'a ConcurrencyLimiter' do
-      it { expect(limiter).to be_a Berater::ConcurrencyLimiter }
+      it { expect(subject).to be_a Berater::ConcurrencyLimiter }
 
       it 'checks arguments' do
         expect {
@@ -147,12 +147,12 @@ describe 'Berater.test_mode' do
 
       it 'works per usual' do
         expect(Berater::ConcurrencyLimiter::LUA_SCRIPT).to receive(:eval).twice.and_call_original
-        expect(limiter.limit).to be_a Berater::Lock
-        expect { limiter.limit }.to be_overloaded
+        expect(subject.limit).to be_a Berater::Lock
+        expect { subject.limit }.to be_overloaded
       end
 
       it 'yields per usual' do
-        expect {|block| limiter.limit(&block) }.to yield_control
+        expect {|block| subject.limit(&block) }.to yield_control
       end
     end
 
@@ -163,8 +163,8 @@ describe 'Berater.test_mode' do
 
       it 'always works and without calling redis' do
         expect(Berater::ConcurrencyLimiter::LUA_SCRIPT).not_to receive(:eval)
-        expect {|block| limiter.limit(&block) }.to yield_control
-        10.times { expect(limiter.limit).to be_a Berater::Lock }
+        expect {|block| subject.limit(&block) }.to yield_control
+        10.times { expect(subject.limit).to be_a Berater::Lock }
       end
     end
 
@@ -175,7 +175,7 @@ describe 'Berater.test_mode' do
 
       it 'never works and without calling redis' do
         expect(Berater::ConcurrencyLimiter::LUA_SCRIPT).not_to receive(:eval)
-        expect { limiter }.to be_overloaded
+        expect { subject }.to be_overloaded
       end
     end
   end
