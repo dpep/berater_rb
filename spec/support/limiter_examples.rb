cfb1dc6bae86d816e56af6c54bf7c03deeaea0b3
Daniel Pepper <pepper.daniel@gmail.com>
2021-03-04 07:34:51 -0800

    test coverage

--- spec/support/limiter_examples.rb
+++ spec/support/limiter_examples.rb
@@ -55,4 +55,18 @@ RSpec.shared_examples 'a limiter' do |limiter|
     end
   end
 
+  describe '#overloaded?' do
+    it 'indicates whether limit will be successful or not' do
+      if limiter.overloaded?
+        expect { limiter.limit }.to be_overloaded
+      else
+        expect(limiter.limit).to be_a Berater::Lock
+      end
+    end
+  end
+
+  describe '#to_s' do
+    it { expect(limiter.to_s).to match(/Berater/) }
+  end
+
 end
